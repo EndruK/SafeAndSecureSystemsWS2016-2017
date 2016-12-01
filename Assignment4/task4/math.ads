9fbaa2979ac4bd9912e8191919fee2b53081b60b
g.luensdorf <g.luensdorf@gmail.com>
2016-12-01 19:02:12 +0100

    more hoare stuff

--- Assignment4/task4/math.ads
+++ Assignment4/task4/math.ads
@@ -10,64 +10,83 @@ package body Math is
     type Unsigned is mod 2**(Integer'Size);
 
     -- precondition(implicit) := X, Y >= 0 and X, Y <= Natural'Last
-    -- postcondition := X = Y'Old and Y = X'Old
+    -- postcondition := X = Y'Old and Y = X'Old and (X, Y) is Permutation(X'Old, Y'Old)
     Procedure S(X: in out Natural; Y: in out Natural) is
     -- X and Y are Natural -> 2**31 - 1
     begin -- def(X, Y);
-        X := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(X); Rule: Assignment
-        Y := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(Y); Rule: Assignment and Composition
-        X := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(X); Rule: Assignment and Composition
+        X := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(X);
+        Y := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(Y);
+        X := Natural(Unsigned(X) xor Unsigned(Y)); -- c-use(X, Y); def(X);
 
-    --  precondition(implicit): X, Y >= 0 and X, Y <= Natural'Last (given through subtype Natural)
-    --  to proof: X = Y'Old and Y = X'Old
-
-    -- [X = X'Old, Y = Y'Old]              X := X xor Y; [X = X'Old xor Y'Old and Y = Y'Old]
-    -- [X = X'Old xor Y'Old and Y = Y'Old] Y := X xor Y; [X = X'Old xor Y'Old and Y = X'Old]
-    -- [X = X'Old xor Y'Old and Y = X'Old] X := X xor Y; [X = Y'Old and Y = X'Old]
-    -- [X = X'Old, Y = Y'Old] S; [X = Y'Old and Y = X'Old]
-    -- proofed: X = Y'Old and Y = X'Old
+    -- Rule: Assignment and Composition
+    -- [X = X'Old and Y = Y'Old]           X := X xor Y; [X = X'Old xor Y'Old and Y = Y'Old] -- Assignment
+    -- [X = X'Old xor Y'Old and Y = Y'Old] Y := X xor Y; [X = X'Old xor Y'Old and Y = X'Old] -- Assignment
+    -- [X = X'Old xor Y'Old and Y = X'Old] X := X xor Y; [X = Y'Old and Y = X'Old] -- Assignment
+    -- [X = X'Old and Y = Y'Old] S; [X = Y'Old and Y = X'Old]
     end S;
 
+
+
+
+
     -- precondition(implicit) := X, Y, Z >= 0 and X, Y, Z <= Natural'Last
-    -- postcondition := (X <= Y <= Z)
-    --    and(
-    --           (X = X'Old and Y = Y'Old and Z = Z'Old)
-    --        or (X = X'Old and Z = Y'Old and Y = Z'Old)
-    --        or (Y = X'Old and X = Y'Old and Z = Z'Old)
-    --        or (Y = X'Old and Z = Y'Old and X = Z'Old)
-    --        or (Z = X'Old and X = Y'Old and Y = Z'Old)
-    --        or (Z = X'Old and Y = Y'Old and X = Z'Old)
-    --    )
+    -- postcondition := (X <= Y <= Z) and (X, Y, Z) is Permutation(X'Old, Y'Old, Z'Old)
 
     Procedure T(X: in out Natural; Y: in out Natural; Z: in out Natural) is
     begin -- def (X, Y, Z);
 
-        Given: X < Y ==> X <= Y
+        -- Define Permutation := (X, Y, Z) is Permutation(X'Old, Y'Old, Z'Old)
+        -- Given: X < Y ==> X <= Y
 
-        precondition: [] -- Rules: IF-THEN-ELSE, Assignment, Empty Statement
-        [not C1] null; [X <= Y]
-        [C1] S; [X <= Y]
-        postcondition: [X <= Y]
+        precondition: [Permutation] -- Rules: IF-THEN-ELSE, Assignment, Empty Statement, Conclusion
+        [not C1 and Permutation] null; [X <= Y and Permutation]
+        [C1 and Permutation] S; [X <= Y and Permutation]
+        postcondition: [X <= Y and Permutation]
+
+IF-THEN-ELSE{
+    precondition: [Permutation]
+        [not C1] null; [X <= Y]{
+            Conclusion and Empty Statement
+        }
+        [C1] S; [X <= Y]{
+            Assignment and Conclusion
+        }
+    postcondition: [X <= Y and Permutation]
+}
 
         if X > Y then -- p-use(X, Y)
             S(X, Y); -- c-use(X, Y); def(X, Y)
         end if;
 
-        precondition: [X <= Y] -- Rules: IF-THEN-ELSE, Assignment, Composition, Empty Statement
-        [not C2 and X <= Y] null; [X <= Z and Y <= Z]
-            [not C2 and X <= Y] ==> [X <= Z and Y <= Z]
-                [Y <= Z and X <= Y] ==> [X <= Z and Y <= Z]
-        [C2 and X <= Y] S; [X <= Z and Y <= Z]
-        postcondition: [X <= Z and Y <= Z]
+        precondition: [X <= Y and Permutation] -- Rules: IF-THEN-ELSE, Assignment, Empty Statement, Conclusion
+        [not C2 and X <= Y and Permutation] null; [X <= Z and Y <= Z and Permutation] -- {P'}S{Q'} (Empty Statement)
+            [not C2 and X <= Y and Permutation] ==> [X <= Z and Y <= Z and Permutation] -- P ==> P', see Conclusion
+                [Y <= Z and X <= Y and Permutation] ==> [X <= Z and Y <= Z and Permutation]
+        [C2 and X <= Y and Permutation] S; [X <= Z and Y <= Z and Permutation] -- need a Conclusion
+            [Y > Z and X <= Y and Permutation] S(Y, Z); [X <= Z and Y <= Z and Permutation] -- need a Conclusion
+            P = P' := Y > Z and X <= Y and Permutation
+                Q' := Z > Y and X <= Z and Permutation  -- Assignment: {P'}S;{Q'} -> Conclusion {P}S;{Q}
+                Q := X <= Z and Y <= Z and Permutation
+        postcondition: [X <= Z and Y <= Z and Permutation]
 
+IF-THEN-ELSE{
+    precondition: [X <= Y and Permutation]
+        [X <= Y and not C1 and Permutation] null; [X <= Y and Y <= Z and Permutation]{
+            Conclusion and Empty Statement
+        }
+        [X <= Y and C1 and Permutation] S; [X <= Y and Y <= Z and Permutation]{
+            Assignment and Conclusion
+        }
+    postcondition: [X <= Y and Y <= Z and Permutation]
+}
         if Y > Z then -- p-use(Y, Z)
             S(Y, Z); -- c-use(Y, Z); def(Y, Z)
         end if;
 
-        precondition: [X <= Z and Y <= Z] -- Rules: IF-THEN-ELSE, Assignment, Composition, Empty Statement
-        [not C3 and X <= Z and Y <= Z] null; [X <= Y and Y <= Z and X <= Z]
-        [C3 and X <= Z and Y <= Z] S; [X <= Y and Y <= Z and X <= Z]
-        postcondition: [ X <= Y and Y <= Z]
+        precondition: [X <= Z and Y <= Z and Permutation] -- Rules: IF-THEN-ELSE, Assignment, Empty Statement, Conclusion
+        [not C3 and X <= Z and Y <= Z and Permutation] null; [X <= Y and Y <= Z and X <= Z and Permutation]
+        [C3 and X <= Z and Y <= Z and Permutation] S; [X <= Y and Y <= Z and X <= Z and Permutation]
+        postcondition: [ X <= Y and Y <= Z and Permutation]
 
         if X > Y then -- p-use(X, Y)
             S(X, Y); -- c-use(X, Y); def(X, Y)
