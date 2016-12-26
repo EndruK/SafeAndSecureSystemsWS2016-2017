f9678613bbe6aff6533cee0b35ddc3e99b6482c4
g.luensdorf <g.luensdorf@gmail.com>
2016-12-26 15:29:26 +0100

    und dann trittst du mode=prove voll ausm bild

--- Assignment6/task2/src/sorting.adb
+++ Assignment6/task2/src/sorting.adb
@@ -17,26 +17,23 @@ package body Sorting is
                 and (A'Length > 1) and (A'Length <= Integer'Last)
                 and (iMin in A'Range)
             );
-            -- pragma Loop_Variant(Decreases => (A'Length - 1) - j);
-            -- pragma Loop_Invariant((iMin in A'Range) and (for all x in A'First .. j => A(x) <= A(j)));
             -- Iterate over all elements in A from 'j+1' until A'Last and find smallest value of them
             -- iMin holds index of smallest found value
             -- After iterating over all elements swap values of A(j) with A(iMin)
             for i in j + 1 .. A'Last loop
                 pragma Loop_Invariant(
                     (i in A'Range) and (j+1 in A'Range)
+                    and (iMin <= A'Last) and (iMin in A'Range)
                 );
-                --pragma Loop_Variant(Decreases => A'Last - i);
                 if A(i) < A(iMin) then
                     iMin := i;
                 end if;
             end loop;
-            if iMin /= j then
+            if iMin /= j then -- swap values if not same index
                 temp := A(j);
                 A(j) := A(iMin);
                 A(iMin) := temp;
-            end if;
-            --pragma Loop_Invariant(for all x in A'First .. j => A(x) <= A(j));
+            end if; -- end of swap
         end loop;
     end Selection_Sort;
 end Sorting;
