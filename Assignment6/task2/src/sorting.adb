4dd804e8105152d234323b69c9eb40de77df01c6
g.luensdorf <g.luensdorf@gmail.com>
2017-01-08 17:08:50 +0100

    warning: requires Is_Sorted(A)

--- Assignment6/task2/src/sorting.adb
+++ Assignment6/task2/src/sorting.adb
@@ -10,7 +10,7 @@ package body Sorting is
         -- outer loop, iterates over all elements from First to (Last_Element - 1)
         -- Assume that after each sort the next selected 'j' is the minimum
         -- In the first iteration although
-        for j in A'First .. A'Last - 1 loop --First .. A'Length - 1 loop
+        for j in A'First .. A'Last - 1 loop
             iMin := j;
             pragma Loop_Invariant(
                 (j in A'Range) and (j <= A'Last - 1)
@@ -23,17 +23,31 @@ package body Sorting is
             for i in j + 1 .. A'Last loop
                 pragma Loop_Invariant(
                     (i in A'Range) and (j+1 in A'Range)
-                    and (iMin <= A'Last) and (iMin in A'Range)
+                    and (iMin in A'Range)
                 );
                 if A(i) < A(iMin) then
                     iMin := i;
                 end if;
             end loop;
-            if iMin /= j then -- swap values if not same index
+            if iMin /= j then -- swap values if they do not have the same index
                 temp := A(j);
                 A(j) := A(iMin);
                 A(iMin) := temp;
             end if; -- end of swap
         end loop;
     end Selection_Sort;
+
+    function Is_Sorted(A: in Natural_Array) return Boolean is
+        Result : Boolean := False;
+    begin --Is_Sorted
+        for x in A'First .. A'Last - 1 loop
+            if A(x) <= A(x+1) then
+                Result := True;
+            else
+                Result := False;
+                return Result;
+            end if;
+        end loop;
+        return Result;
+    end Is_Sorted;
 end Sorting;
--- Assignment6/task2/src/sorting.ads
+++ Assignment6/task2/src/sorting.ads
@@ -1,11 +1,21 @@
 -- André Karge 110033
 -- K. Gerrit Lünsdorf 100141
 pragma SPARK_Mode(on);
+
 package Sorting is
+
     type Natural_Array is array(Natural range <>) of Natural;
 
     procedure Selection_Sort(A: in out Natural_Array) with
         Pre =>  A'Length > 1 and
-                A'Length < Integer'Last - 1; --,
-        -- Post => (for all x in A'First .. A'Length - 1 => A(x) <= A(A'Last));
+                A'Length < Integer'Last - 1,
+        Post => Is_Sorted(A) and A'Length = A'Old'Length;
+    --    Post => (for all x in A'Old'Range => (
+    --                if x <= A'Old'Last - 1 then A(x) <= A(x+1)
+    --            ));
+
+    -- helper functions
+    function Is_Sorted(A: in Natural_Array) return Boolean with
+        Pre =>  A'Length > 1 and
+                A'Length < Integer'Last - 1;
 end Sorting;
