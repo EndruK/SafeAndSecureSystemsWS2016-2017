d8032c902397ecb76770d4e5dbfe79dad1fc43e9
g.luensdorf <g.luensdorf@gmail.com>
2017-01-08 22:23:35 +0100

    no proper postcondition, but no idea how to so

--- Assignment6/task2/src/sorting.adb
+++ Assignment6/task2/src/sorting.adb
@@ -4,50 +4,46 @@
 pragma SPARK_Mode(on);
 package body Sorting is
     procedure Selection_Sort(A: in out Natural_Array) is
-        temp : Natural;
-        iMin : Natural;
+        Temp : Natural;
+        I_Min : Natural;
     begin -- Selection_Sort
         -- outer loop, iterates over all elements from First to (Last_Element - 1)
-        -- Assume that after each sort the next selected 'j' is the minimum
+        -- Assume that after each sort the next selected 'J' is the minimum
         -- In the first iteration although
-        for j in A'First .. A'Last - 1 loop
-            iMin := j;
+        for J in A'First .. A'Last - 1 loop
+            I_Min := J;
             pragma Loop_Invariant(
-                (j in A'Range) and (j <= A'Last - 1)
+                (J in A'Range) and (J <= A'Last - 1)
                 and (A'Length > 1) and (A'Length <= Integer'Last)
-                and (iMin in A'Range)
+                and (I_Min in A'Range)
             );
-            -- Iterate over all elements in A from 'j+1' until A'Last and find smallest value of them
-            -- iMin holds index of smallest found value
-            -- After iterating over all elements swap values of A(j) with A(iMin)
-            for i in j + 1 .. A'Last loop
+            -- Iterate over all elements in A from 'J+1' until A'Last and find smallest value of them
+            -- I_Min holds index of smallest found value
+            -- After iterating over all elements swap values of A(J) with A(I_Min)
+            for I in J + 1 .. A'Last loop
                 pragma Loop_Invariant(
-                    (i in A'Range) and (j+1 in A'Range)
-                    and (iMin in A'Range)
+                    (I in A'Range) and (J+1 in A'Range)
+                    and (I_Min in A'Range)
                 );
-                if A(i) < A(iMin) then
-                    iMin := i;
+                if A(I) < A(I_Min) then
+                    I_Min := I;
                 end if;
             end loop;
-            if iMin /= j then -- swap values if they do not have the same index
-                temp := A(j);
-                A(j) := A(iMin);
-                A(iMin) := temp;
+            if I_Min /= J then -- swap values if they do not have the same index
+                Temp := A(J);
+                A(J) := A(I_Min);
+                A(I_Min) := Temp;
             end if; -- end of swap
         end loop;
     end Selection_Sort;
 
-    function Is_Sorted(A: in Natural_Array) return Boolean is
-        Result : Boolean := False;
+    function Is_Sorted(Unsorted_A: in Natural_Array) return Boolean is
     begin --Is_Sorted
-        for x in A'First .. A'Last - 1 loop
-            if A(x) <= A(x+1) then
-                Result := True;
-            else
-                Result := False;
-                return Result;
+        for x in Unsorted_A'First .. Unsorted_A'Last - 1 loop
+            if Unsorted_A(x) > Unsorted_A(x+1) then
+                return False;
             end if;
         end loop;
-        return Result;
+        return True;
     end Is_Sorted;
 end Sorting;
--- Assignment6/task2/src/sorting.ads
+++ Assignment6/task2/src/sorting.ads
@@ -1,21 +1,18 @@
 -- André Karge 110033
 -- K. Gerrit Lünsdorf 100141
-pragma SPARK_Mode(on);
 
 package Sorting is
+    pragma SPARK_Mode(on);
 
     type Natural_Array is array(Natural range <>) of Natural;
 
     procedure Selection_Sort(A: in out Natural_Array) with
         Pre =>  A'Length > 1 and
                 A'Length < Integer'Last - 1,
-        Post => Is_Sorted(A) and A'Length = A'Old'Length;
-    --    Post => (for all x in A'Old'Range => (
-    --                if x <= A'Old'Last - 1 then A(x) <= A(x+1)
-    --            ));
+        Post => A'Length = A'Old'Length;
 
     -- helper functions
-    function Is_Sorted(A: in Natural_Array) return Boolean with
-        Pre =>  A'Length > 1 and
-                A'Length < Integer'Last - 1;
+    function Is_Sorted(Unsorted_A: in Natural_Array) return Boolean with
+        Pre =>  Unsorted_A'Length > 1 and
+                Unsorted_A'Length < Integer'Last - 1;
 end Sorting;
