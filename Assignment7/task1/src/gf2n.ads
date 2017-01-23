85f0d973ed677cdfbd01f64d5829269940f34124
g.luensdorf <g.luensdorf@gmail.com>
2017-01-23 12:02:15 +0100

    with old code

--- Assignment7/task1/src/gf2n.ads
+++ Assignment7/task1/src/gf2n.ads
@@ -1,6 +1,6 @@
 generic
     -- Example: A type GF_2_4_Type is mod 2**4 means that we consider the
-    -- elements of Galois Field (2**8).
+    -- elements of Galois Field (2**4).
     type Element_Type is mod <>;
 
     -- An irreducible polynomial P required for arithmetics.
@@ -11,19 +11,15 @@ package GF2n is
     function "*"(X: Element_Type; Y: Element_Type) return Element_Type;
     function "/"(X: Element_Type; Y: Element_Type) return Element_Type;
 
-    function Shift_Left
-        (Value : Element_Type;
-         Amount: Natural)
-         return Element_Type;
-    pragma Import (Intrinsic, Shift_Left);
-
-    function Shift_Right
-        (Value : Element_Type;
-         Amount: Natural)
-         return Element_Type;
-    pragma Import (Intrinsic, Shift_Right);
-
     function Find_Inverse(X: Element_Type) return Element_Type;
     function GCD(X: Element_Type; Y: Element_Type) return Element_Type;
     function Is_Primitive(X: Element_Type) return Boolean;
+
+    -- Helper functions:
+    -- function Get_Bit(Target: In Element_Type) return String;
+    -- function Get_Order(Target: In Element_Type) return Integer;
+
+    -- function Shift_Left (Value : Element_Type; Amount: Natural) return Element_Type;
+
+    -- function Shift_Right (Value : Element_Type; Amount: Natural) return Element_Type;
 end GF2n;
--- Assignment7/task1/src/main.adb
+++ Assignment7/task1/src/main.adb
@@ -1,29 +1,39 @@
-with Ada.Text_IO;
-with gf2n;
+with Ada.Text_IO, gf2n;
 
 procedure Main is
 
-    type GF_2n is mod 2**8;
+    type GF_2n is mod 2**4;
 
     package GF is new GF2n (
         Element_Type => GF_2n,
-        P => 2#00011011#
+        P => 2#0011#
     );
 
-    X : constant GF_2n := 2#00000101#;
-    Y : constant GF_2n := 2#00000011#;
+    Y : constant GF_2n := 2#1010#;
+    X : constant GF_2n := 2#0011#;
+    -- X : constant GF_2n := 2#01010011#;
+    -- Y : constant GF_2n := 2#11001010#;
     Temp : GF_2n;
+    Temp_Bool : Boolean;
 
 begin
-    Ada.Text_IO.Put_Line(X);
-
     Ada.Text_IO.Put_Line("########### MAIN ###########");
     Temp := GF."+"(X,Y);
     Ada.Text_IO.Put_Line(Temp'Image);
+
     Temp := GF."-"(X,Y);
     Ada.Text_IO.Put_Line(Temp'Image);
-    Temp := GF."*"(X,Y);
-    Ada.Text_IO.Put_Line(Temp'Image);
+
     Temp := GF."/"(X,Y);
     Ada.Text_IO.Put_Line(Temp'Image);
+
+    Temp := GF."*"(X, Y);
+    Ada.Text_IO.Put_Line(Temp'Image);
+
+    Temp := GF.GCD(X, Y);
+    Ada.Text_IO.Put_Line(Temp'Image);
+
+    Temp := GF.Find_Inverse(X);
+    Ada.Text_IO.Put_Line(Temp'Image);
+    Temp_Bool := GF.Is_Primitive(X);
 end Main;
